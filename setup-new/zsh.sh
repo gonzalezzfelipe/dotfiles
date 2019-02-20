18ccab08cd6f11b038b93c5d3e75f037333a88b3
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2019-02-20 00:55:31 -0300

    Initial improvement to zsh

--- setup-new/zsh.sh
+++ setup-new/zsh.sh
@@ -5,10 +5,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # BulletTrain
-mkdir -p $ZSH_CUSTOM/themes/
+mkdir -p /Users/felipe/.oh-my-zsh/themes
 curl \
   http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme \
-  -o $ZSH_CUSTOM/themes/bullet-train.zsh-theme
+  -o /Users/felipe/.oh-my-zsh/themes/bullet-train.zsh-theme
 
 # Required fonts
 git clone https://github.com/powerline/fonts.git --depth=1
