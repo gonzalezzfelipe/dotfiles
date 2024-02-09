c5cbd63d670e569d1756f9d4e54faed1d421c171
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-02-09 17:49:53 -0300

    Add brewfile

--- setup-new/brew.sh
+++ setup-new/brew.sh
@@ -18,67 +18,13 @@ fi
 echo "Updating Brew..."
 brew update && brew upgrade
 
-# Latest zsh
-brew install zsh zsh-completions
+# Use bundle
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+brew bundle --file $current_dir/Brewfile
+
+# zsh
 sudo -s 'echo /usr/local/bin/zsh >> /etc/shells'
 sudo chsh -s /usr/local/bin/zsh
-
-# Git
-brew install git
-
-# Compiler related
-brew install gcc
-brew install llvm
-brew install libomp
-brew install openblas
-brew install coreutils  # (realpath, etc)
-
-brew install python3
-
-# Tmux latest versions
-brew install --HEAD tmux
-
-# Databases
-brew install postgresql
-
-# Other useful binaries
-brew install the_silver_searcher
-brew install fzf
-brew install z
-brew install htop
-if [[  "$OSTYPE" == 'darwin'* ]]; then
-    brew install reattach-to-user-namespace
-    brew install rmtrash
-fi
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
-brew install unrar
-brew install shellcheck
-brew install imgcat
-brew install rsync
-brew install bat
-brew install fd
-brew install tree
-brew install ack-grep
-brew install jq
-brew install orc-tools
-
-# Cask
-brew tap caskroom/cask
-brew install --cask rectangle
-brew install --cask keepingyouawake
-brew install --cask monitorcontrol
-
-# Docker
-brew install --cask docker docker-compose
-
-# Nvim
-brew install nvim
-
-brew tap homebrew/cask-fonts
-# Nerd fonts Source Code Pro version doesn't have italics so we install
-# the official version
-brew install font-source-code-pro
-brew install font-fantasque-sans-mono
 
 # Remove outdated versions
 brew cleanup
