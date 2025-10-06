c847d819f5c2993d04c4e53d624c83f5d5578e8e
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-10-06 14:03:39 -0300

    Several fixes from update

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,53 +1,14 @@
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local configs = require('lspconfig/configs')
-local util = require('lspconfig/util')
-local path = util.path
-
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-
-  -- Fallback to system Python.
-  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
-end
-
-lspconfig.pyright.setup({
-  before_init = function(_, config)
-    config.settings.python.pythonPath = get_python_path(config.root_dir)
-  end,
+vim.lsp.config('terraformls', {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "python" },
+  settings = {
+    terraform = {
+      validate = true
+    }
+  }
 })
 
-lspconfig.terraformls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "tf", "terraform" },
-})
-
-lspconfig.prismals.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-
-lspconfig.dockerls.setup {
-  before_init = function(params)
-    params.processId = vim.NIL
-  end,
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+vim.lsp.enable('terraformls')
